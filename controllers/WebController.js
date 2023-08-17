840740a91a509b40179e7c710258cd406475a5f3
Muhammad Soleh <matthelosh@gmail.com>
2023-08-17 08:43:29 +0700

    Tes req headers

--- controllers/WebController.js
+++ controllers/WebController.js
@@ -1,5 +1,6 @@
 const web = {
     home: (req, res) => {
+        console.log(req.headers);
         res.render('index', {title: 'Selamat Datang', message: 'Salam. Silahkan memanfaatkan API Whatsapp Mandita', headers: req.header('User-Agent')});
     }
 }
